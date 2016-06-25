3439a590f111701550b8c87ad4256a5bfc6c47d4
sargentstudley <mattwt.studley@gmail.com>
2016-06-24 20:13:44 -0400

    - Major reorganization of folder structure to put original code next to new code library.

---
+++ DotNetLicense.UnitTests/LicenseManagerTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using DotNetLicense;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetLicense.UnitTests
+{
+    [TestClass]
+    public class LicenseManagerTests
+    {
+        private string _classTestDirName;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _classTestDirName = "dotnetlicensingworkdir";
+            Directory.CreateDirectory(_classTestDirName);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Directory.Delete(this._classTestDirName, true);
+        }
+
+        [TestMethod]
+        public void CreateNewKeyPair()
+        {
+            LicenseManager manager = new LicenseManager();
+            manager.CreateKeyPairs(_classTestDirName, "testNewPair");
+
+            string expectedPublicKeyPath = string.Format("{0}\\testNewPair_public.key",_classTestDirName);
+            string expectedPrivateKeyPath = string.Format("{0}\\testNewPair_private.key",_classTestDirName);
+            Assert.IsTrue(File.Exists(expectedPublicKeyPath));
+            Assert.IsTrue(File.Exists(expectedPrivateKeyPath));   
+        }
+
+        [TestMethod]
+        public void LoadPrivateKeyFromString()
+        {
+            LicenseManager manager = new LicenseManager();
+            manager.LoadPrivateKeyFromString(TestKeys.PrivateKey);
+            Assert.IsNotNull(manager.PrivateKey);
+        }
+
+        [TestMethod]
+        public void LoadPublicKeyFromString()
+        {
+            LicenseManager manager = new LicenseManager();
+            manager.LoadPublicKeyFromString(TestKeys.PublicKey);
+            Assert.IsNotNull(manager.PublicKey);
+        }
+
+        [TestMethod]
+        public void CreateNewLicense()
+        {
+            LicenseManager manager = new LicenseManager();
+            manager.LoadPrivateKeyFromString(TestKeys.PrivateKey);
+            string expectedFilePath = string.Format("{0}\\testLicense.lic", _classTestDirName);
+            manager.SignAndSaveNewLicense(TestLicenses.GetTestUnsignedLicense(), expectedFilePath);
+
+            Assert.IsTrue(File.Exists(expectedFilePath));
+        }
+
+        [TestMethod]
+        public void LoadSignedLicense()
+        {
+            LicenseManager manager = new LicenseManager();
+            manager.LoadPublicKeyFromString(TestKeys.PublicKey);
+            License testLicense = manager.LoadLicenseFromString(TestLicenses.GetTestSignedLicenseString());
+
+            Assert.IsNotNull(testLicense);
+        }
+
+    }
+}
---
+++ DotNetLicense.UnitTests/TestLicenses.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetLicense.UnitTests
+{
+    internal static class TestLicenses
+    {
+        internal static License GetTestUnsignedLicense()
+        {
+            License license = new License();
+            license.AddOrChangeAttribute("Company", "Test Co.");
+            license.AddOrChangeAttribute("LicensedOn", DateTime.Now.ToShortDateString());
+
+            return license;
+        }
+
+        internal static string GetTestSignedLicenseString()
+        {
+            string licenseText = File.ReadAllText("testSignedLicense.lic");
+            return licenseText;
+        }
+    }
+}
